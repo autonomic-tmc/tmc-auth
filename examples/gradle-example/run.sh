6af0934c9e6d68126889304c2f61fdf2c87d0db9
falguni-integral <falguni@autonomic.ai>
2020-03-11 16:58:14 -0400

    Update examples/gradle-example/run.sh
    Co-Authored-By: matthew-auto <38868365+matthew-auto@users.noreply.github.com>

--- examples/gradle-example/run.sh
+++ examples/gradle-example/run.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 set -o errexit
 
-if [ $# -eq 0 ]
-  then
-    ./gradlew run
-  else
-    ./gradlew run -Ptmc-auth.version=$1
+if [ $# -eq 0 ]; then
+  ./gradlew run
+else
+  ./gradlew run -Ptmc-auth.version="$1"
 fi
