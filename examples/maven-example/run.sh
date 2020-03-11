86275e66a92e642c364d18d7f0344b57c6b2f4b2
falguni-integral <falguni@autonomic.ai>
2020-03-11 16:57:03 -0400

    Update examples/maven-example/run.sh
    Co-Authored-By: matthew-auto <38868365+matthew-auto@users.noreply.github.com>

--- examples/maven-example/run.sh
+++ examples/maven-example/run.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 set -eo errexit
 
-if [ $# -eq 0 ]
-  then
-    ./mvnw -s ./settings.xml spring-boot:run
-  else
-    ./mvnw -s ./settings.xml spring-boot:run -Dtmc-auth.version="$1"
+if [ $# -eq 0 ]; then
+  ./mvnw -s ./settings.xml spring-boot:run
+else
+  ./mvnw -s ./settings.xml spring-boot:run -Dtmc-auth.version="$1"
 fi
