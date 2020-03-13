882eb90d23c274bbac2eae90b2c1f5276b1081b8
Integer-Andrew <andrew@integral.io>
2020-03-13 16:28:10 -0400

    Feat: also verify in gradle

--- examples/maven-example/src/test/java/com/autonomic/tmc/example/auth/MavenAuthExampleTest.java
+++ examples/maven-example/src/test/java/com/autonomic/tmc/example/auth/MavenAuthExampleTest.java
@@ -1,18 +1,55 @@
 package com.autonomic.tmc.example.auth;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.ArgumentMatchers.eq;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Captor;
+import org.mockito.Mockito;
+import org.mockito.Spy;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-public class MavenAuthExampleTest {
+class MavenAuthExampleTest {
 
     @Autowired
-    MavenAuthExample ex;
+    private MavenAuthExample example;
+
+    @Captor
+    private ArgumentCaptor<Exception> errorMessages;
+
+    @Spy
+    LoggerStub loggerStub;
+
     @Test
-    public void givenBadCredentials_whenAuthenticating_thenVerifyErrorMessageContainsSdkVersion() {
-        ex.run("hi");
+    void givenBadCredentials_whenAuthenticating_thenVerifyErrorMessageContainsSdkVersion() {
+        example.LOGGER = loggerStub;
+        example.run("hi");
+        Mockito.verify(example.LOGGER).log(
+            eq(Level.SEVERE),
+            eq("Generic message, Something went wrong: "),
+            errorMessages.capture());
+        String actualError = errorMessages.getAllValues().get(0).getMessage();
+
+        assertTrue(actualError.contains("tmc-auth"));
+        assertTrue(actualError.contains("-SERVICE"));
+        assertTrue(actualError.contains("Authorization failed for user [NOTAREALCLIENTID]"));
+    }
+
+    private static class LoggerStub extends Logger {
+
+        public LoggerStub() {
+            super(LoggerStub.class.toString(), null);
+        }
+
+        @Override
+        public void log(Level level, String message, Throwable e) {
+            super.log(level, message, e);
+        }
     }
 
 }
