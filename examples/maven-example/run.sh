7a2944d1b634dc43de2b85ecd1269a1e6ba798c8
Karthi Thananjayan <60152313+karthi-au@users.noreply.github.com>
2020-04-29 23:25:40 -0400

    Update examples/maven-example/run.sh
    Replace mvn with ./mvnw.
    
    Co-Authored-By: Aimme Keener <38669990+AimmeK@users.noreply.github.com>

--- examples/maven-example/run.sh
+++ examples/maven-example/run.sh
@@ -2,7 +2,7 @@
 set -eo errexit
 
 if [ $# -eq 0 ]; then
-  mvn -s ./settings.xml spring-boot:run
+  ./mvnw -s ./settings.xml spring-boot:run
 else
   ./mvnw -s ./settings.xml spring-boot:run -Dtmc-auth.version="$1"
 fi
