6f1acb753cd5158c4a6f88ca09529fc46a51ac84
Jason Fedorowich <102973456+jasonfedorowic-ai@users.noreply.github.com>
2026-03-25 13:26:06 -0400

    add config for jfrog

--- src/main/java/com/autonomic/tmc/auth/Token.java
+++ src/main/java/com/autonomic/tmc/auth/Token.java
@@ -4,7 +4,7 @@ package com.autonomic.tmc.auth;
  * ‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾
  * tmc-auth
  * ——————————————————————————————————————————————————————————————————————————————
- * Copyright (C) 2016 - 2024 Autonomic, LLC
+ * Copyright (C) 2016 - 2026 Autonomic, LLC
  * ——————————————————————————————————————————————————————————————————————————————
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not
  * use this file except in compliance with the License. You may obtain a copy of
